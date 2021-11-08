f526c15c92402632aac1172d9e282a5c81e4c8cd
Micbro-fau <micahbrown2016@fau.edu>
2021-11-08 13:05:56 -0500

    MySQL Settings (Not Working ATM)

--- checkersAI/settings.py
+++ checkersAI/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'checkersAI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'micahbrown2016',
+        'USER': 'micahbrown2016',
+        'PASSWORD': 'd5K6TKUtf4',
+        'HOST': 'lamp.cse.fau.edu',
+        'PORT': '32',
     }
 }
 
