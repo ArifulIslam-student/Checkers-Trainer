8a161000db651f24d61daf83771405139fd7ee36
ArifulIslam-student <64557185+ArifulIslam-student@users.noreply.github.com>
2021-12-06 16:13:33 -0500

    created another usertable that has ai difficulty

--- checkersAI/models.py
+++ checkersAI/models.py
@@ -5,3 +5,12 @@ from django.db import models
 class Users(models.Model):
     userid = models.CharField(max_length=100)
     userpassword = models.CharField(max_length=100)
+
+
+class Userstable(models.Model):
+    userid = models.CharField(max_length=100)
+    userpassword = models.CharField(max_length=100)
+    aidifficulty = models.IntegerField()
+
+    class Meta:
+        db_table = "usertable"
